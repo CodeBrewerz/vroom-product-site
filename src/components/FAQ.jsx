f651165ced0dd6c9d4faa03a897d830cf5d9dcca
Sumit Mittal <sumit22mittal@gmail.com>
2022-02-25 16:07:28 -0500

    add comparison and FAQ section

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,63 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Disclosure } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+
+const faqs = [
+  {
+    question: "How is this different than ZipCar?",
+    answer:
+      "Zipcar has a Same Pickup and Same Dropoff policy, we don't have that policy.  ",
+  },
+  {
+    question: "When can I expect more information coming out?",
+    answer:
+      "We are working on assembling different parts of the system, and will send an email with furthur information.",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function FAQ() {
+  return (
+    <div >
+      <div className="max-w-7xl mx-auto px-4 sm:py-16 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+          <h2 className="text-center text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-blue-600 sm:text-4xl">
+            Frequently asked questions
+          </h2>
+          <dl className="mt-6 space-y-6 divide-y divide-gray-200">
+            {faqs.map((faq) => (
+              <Disclosure as="div" key={faq.question} className="pt-6">
+                {({ open }) => (
+                  <>
+                    <dt className="text-lg">
+                      <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
+                        <span className="font-medium text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-blue-600">
+                          {faq.question}
+                        </span>
+                        <span className="ml-6 h-7 flex items-center">
+                          <ChevronDownIcon
+                            className={classNames(
+                              open ? "-rotate-180" : "rotate-0",
+                              "h-6 w-6 transform"
+                            )}
+                            aria-hidden="true"
+                          />
+                        </span>
+                      </Disclosure.Button>
+                    </dt>
+                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                      <p className="text-base text-white">{faq.answer}</p>
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
